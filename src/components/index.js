0d15b243aaa33c203f00799d55fc634fba86a441
jewlmils <st.jewelmilagrosa@gmail.com>
2024-06-25 02:57:28 +0800

    fix: folder names for better readability

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,5 @@ export { default as ChannelList } from "./MainPage/ChannelApp/ChannelList";
 export { default as Sidebar } from "./MainPage/Sidebar";
 export { default as Dashboard } from "./MainPage/Dashboard";
 export { default as LandingPage } from "./LandingPage";
-export { default as SignIn } from "./SignIn_SignUp/SignIn";
-export { default as SignUp } from "./SignIn_SignUp/SignUp";
+export { default as SignIn } from "./auth/SignIn";
+export { default as SignUp } from "./auth/SignUp";
